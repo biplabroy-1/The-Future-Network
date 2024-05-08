523b539450a61b1d946aa6d38a95c643d81d80e3
Biplab Roy <biplabr119201@gmail.com>
2024-05-08 11:55:05 +0530

    updated header page

--- frontend/src/js/script1.js
+++ frontend/src/js/script1.js
@@ -3,3 +3,10 @@ console.log(logo);
 for (let i = 0; i < logo.length; i++) {
     console.log(`letter ${i} is ${logo[i].getTotalLength()}`);
 }
+
+
+function next() {
+    const nextElement = document.querySelector("#next");
+    nextElement.scrollIntoView({ behavior: "smooth" });
+}
+

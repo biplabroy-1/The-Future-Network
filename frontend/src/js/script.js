acdf5dc83a8743fa57586335c245cde82b6416a7
Biplab Roy <biplabr119201@gmail.com>
2024-05-11 22:18:00 +0530

    "Updated CSS properties, added JS scroll event listener for navbar and logo size changes"
    This commit includes changes to the custom.css file where the background-color property of the navbar class is updated. Additionally, the index.html

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -3,10 +3,9 @@ function test() {
     const spanElement = document.querySelector("#typing")
     spanElement.innerHTML = '<span>Hello</span>';
     setTimeout(function () {
-        spanElement.innerHTML = '<span class="typed-work"></span > ';
+        spanElement.innerHTML = '<span class="typed-work"></span >';
     }, 1700);
 }
-
 test();
 
 function next() {
@@ -36,10 +35,64 @@ setTimeout(function () {
 
 
 setTimeout(function () {
+    var navbar = document.getElementsByClassName('navbar');
     var btn = document.getElementById('learn-Btn');
     btn.removeAttribute('data-aos');
     btn.removeAttribute('data-aos-duration');
 }, 1500);
+setTimeout(function () {
+    var navbar = document.getElementById('navbar');
+    navbar.removeAttribute('data-aos');
+    navbar.removeAttribute('data-aos-duration');
+}, 1500);
+
+
+// window.addEventListener('scroll', function () {
+//     var navbar = document.getElementById('navbar');
+//     var logo = document.getElementById('logo');
+
+//     if (window.scrollY > 0) {
+//         navbar.classList.remove('h-24');
+//         navbar.classList.add('h-16');
+//         logo.classList.remove('w-32');
+//         logo.classList.add('w-24');
+//     } else {
+//         navbar.classList.remove('h-16');
+//         navbar.classList.add('h-24');
+//         logo.classList.remove('w-24');
+//         logo.classList.add('w-32');
+//     }
+// });
+
+
+
+
+
+
+var prevScrollPos = window.pageYOffset;
+
+window.addEventListener('scroll', function () {
+    var currentScrollPos = window.pageYOffset;
+    var navbar = document.getElementById('navbar');
+    var logo = document.getElementById('logo');
+
+    if (currentScrollPos > prevScrollPos) {
+        // Scrolling up
+        navbar.classList.remove('h-24');
+        navbar.classList.add('h-16');
+        logo.classList.remove('w-32');
+        logo.classList.add('w-24');
+    } else {
+        // Scrolling down
+        navbar.classList.remove('h-16');
+        navbar.classList.add('h-24');
+        logo.classList.remove('w-24');
+        logo.classList.add('w-32');
+    }
+
+    prevScrollPos = currentScrollPos;
+});
+
 
 const button = document.getElementById('toggleButton');
 const boxIcon1 = document.getElementById('boxIcon1');

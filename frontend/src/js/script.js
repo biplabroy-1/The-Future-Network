a0c26ca0c0d68d0dcd78e0a48543b671ca7cf6e3
Biplab Roy <biplabr119201@gmail.com>
2024-05-12 13:49:56 +0530

    Main "Added custom CSS for number inputs, updated navbar hover effect, modified index.html with new logo and added sections in JS for typing animation and smooth scrolling and added new font"

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -1,39 +1,46 @@
-
+// Function to initialize typing animation
 function test() {
-    const spanElement = document.querySelector("#typing")
+    // Select the span element with id "typing"
+    const spanElement = document.querySelector("#typing");
+    // Set innerHTML to display "Hello" inside the span
     spanElement.innerHTML = '<span>Hello</span>';
+    // Set a timeout to change innerHTML after 1700 milliseconds
     setTimeout(function () {
         spanElement.innerHTML = '<span class="typed-work"></span >';
     }, 1700);
 }
+// Call the test function
 test();
 
+// Function to scroll to the next element smoothly
 function next() {
     const nextElement = document.querySelector("#next");
     nextElement.scrollIntoView({ behavior: "smooth" });
 }
 
-
+// Set a timeout function to execute after a delay of 1700 milliseconds
 setTimeout(function () {
+    // Initialize a new Typed object to type strings
     let typed = new Typed('.typed-work', {
+        // Array of strings to be typed
         strings: [
-            'ADVANCING HUMAN SPACEFLIGHT',
-            'STARLINK MISSION',
-            'TO MAKE LIFE MULTIPLANETARY',
-            `STARSHIP'S THIRD FLIGHT TEST`
+            'ADVANCING HUMAN COMMUNICTION',
+            'PROJECT B-CAP',
+            'TO TALK TO AI',
+            'U-MUX 13th TEST'
         ],
+        // Delay before typing starts
         startDelay: 200,
+        // Speed of typing characters
         typeSpeed: 40,
+        // Speed of backspacing characters
         backSpeed: 20,
+        // Whether the typing sequence should loop
         loop: true,
     });
-}, 1700)
+}, 1700);
 
-
-
-
-
-
+// Set timeouts to remove AOS animations after 1500 milliseconds
 setTimeout(function () {
     var navbar = document.getElementsByClassName('navbar');
     var btn = document.getElementById('learn-Btn');
@@ -46,33 +53,10 @@ setTimeout(function () {
     navbar.removeAttribute('data-aos-duration');
 }, 1500);
 
-
-// window.addEventListener('scroll', function () {
-//     var navbar = document.getElementById('navbar');
-//     var logo = document.getElementById('logo');
-
-//     if (window.scrollY > 0) {
-//         navbar.classList.remove('h-24');
-//         navbar.classList.add('h-16');
-//         logo.classList.remove('w-32');
-//         logo.classList.add('w-24');
-//     } else {
-//         navbar.classList.remove('h-16');
-//         navbar.classList.add('h-24');
-//         logo.classList.remove('w-24');
-//         logo.classList.add('w-32');
-//     }
-// });
-
-
-
-
-
-
-var prevScrollPos = window.pageYOffset;
-
+// Handle scrolling event to change navbar and logo size
+var prevScrollPos = window.scrollY;
 window.addEventListener('scroll', function () {
-    var currentScrollPos = window.pageYOffset;
+    var currentScrollPos = window.scrollY;
     var navbar = document.getElementById('navbar');
     var logo = document.getElementById('logo');
 
@@ -93,7 +77,7 @@ window.addEventListener('scroll', function () {
     prevScrollPos = currentScrollPos;
 });
 
-
+// Toggle button functionality to switch between two icons
 const button = document.getElementById('toggleButton');
 const boxIcon1 = document.getElementById('boxIcon1');
 const boxIcon2 = document.getElementById('boxIcon2');

34fa9730c158aa270d55a1ac5e01ba244749e96d
Biplab Roy <biplabr119201@gmail.com>
2024-06-20 04:22:18 +0530

    "Updated CSS files, removed team.card.css, updated index.html with new team section, and added JavaScript code for team section tooltip"

--- frontend/src/js/script.js
+++ frontend/src/js/script.js
@@ -121,3 +121,26 @@ adjustNavbarOnScroll(); // Start monitoring scroll and updating navbar and small
 // Call the function every 200ms
 setInterval(adjustNavbarOnScroll, 200);
 
+
+
+
+// ! Team js
+
+
+var tooltip = document.querySelector(".tooltip-flair");
+window.addEventListener("mousemove", (event) => {
+    tooltip.style.left = `${event.x}px`;
+    tooltip.style.top = `${event.y}px`;
+});
+document.querySelectorAll(".member").forEach((item) => {
+    item.addEventListener("mouseout", () => {
+        tooltip.classList.remove("tooltip-flair__active");
+    });
+    item.addEventListener("mouseover", () => {
+        tooltip.classList.add("tooltip-flair__active");
+        tooltip.querySelector("span").innerText = item.dataset.name;
+    });
+});
+
+
+// ! Team js
